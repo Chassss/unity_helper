db5d023c772741e46c8600804f9b545346dbd8db
Chasss <colinakachas2001@gmail.com>
2026-04-30 23:51:42 -0700

    Add memory module

---
+++ src/unity_helper/memory.py
@@ -0,0 +1,61 @@
+import ctypes
+
+class MEMORY_BASIC_INFORMATION(ctypes.Structure):
+    _fields_ = [
+        ("BaseAddress", ctypes.c_void_p),
+        ("AllocationBase", ctypes.c_void_p),
+        ("AllocationProtect", ctypes.c_ulong),
+        ("RegionSize", ctypes.c_size_t),
+        ("State", ctypes.c_ulong),
+        ("Protect", ctypes.c_ulong),
+        ("Type", ctypes.c_ulong),
+    ]
+
+MEM_COMMIT = 0x1000
+PAGE_NOACCESS = 0x01
+PAGE_GUARD = 0x100
+
+
+_ReadProcessMemory = ctypes.windll.kernel32.ReadProcessMemory
+
+_ReadProcessMemory.argtypes = (ctypes.c_void_p, ctypes.c_void_p, ctypes.c_void_p, ctypes.c_size_t, ctypes.POINTER(ctypes.c_size_t))
+_ReadProcessMemory.restype = ctypes.c_bool
+
+_VirtualQuery = ctypes.windll.kernel32.VirtualQuery
+
+
+def is_64bit():
+    if ctypes.sizeof(ctypes.c_void_p) == 8:
+        return True
+    return False
+
+def read_bytes(address, size):
+    buff = ctypes.create_string_buffer(size)
+
+
+    if not _ReadProcessMemory(ctypes.windll.kernel32.GetCurrentProcess(), address, buff, size, None):
+        return None
+    
+    return buff.raw
+
+
+def get_pages():
+    address = 0
+    limit = 0x7FFFFFFF0000 if is_64bit() else 0x7fff0000
+
+    regions = []
+
+    while address < limit:
+        mbi = MEMORY_BASIC_INFORMATION()
+
+        result = _VirtualQuery(ctypes.c_void_p(address), ctypes.byref(mbi), ctypes.sizeof(mbi))
+
+        if result == 0:
+            break
+
+        if mbi.State == MEM_COMMIT:
+            regions.append((mbi.BaseAddress, mbi.RegionSize, mbi.Protect))
+
+        address += mbi.RegionSize
+
+    return regions
